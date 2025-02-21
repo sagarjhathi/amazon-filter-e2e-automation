25a3fca963901703a49702401957da65af9eaf2c
sagarjhathi <sagarjhathii@gmail.com>
2025-02-22 00:22:10 +0530

    optimizing the code in the base level

--- eclipse-workspace/Intro/src/googleaggregator/Base.java
+++ eclipse-workspace/Intro/src/googleaggregator/Base.java
@@ -88,55 +88,19 @@ public class Base {
         driver.findElement(By.cssSelector("span.lg3aE[title='Amazon.in']")).click();
         
         
-        //Clicking upon the  1st product after clicking the amazon  
-       //driver.findElement(By.xpath("(//span[@class='OA4wid' and text()='Smartphone'])[1]")).click();
-        
-       //Clicking upon view more details after clicking the 1st product  in the above click
-      // driver.findElement(By.xpath("//a[text()='View product details']")).click();
-       
        
        //Creating arraylist to store all the web elements of the below address to locate and store all the prices of the product.
        List<WebElement> list=new ArrayList<>();
        list=driver.findElements(By.xpath("//span[@class='g9WBQb']"));
        
        
-       //Creating an arraylist to store the exact number after parsing and triming down using the extract function called within. 
-//       List<String> gPrices=new ArrayList<>();
-//       for(int i=0;i<list.size();i++) {
-//    	   gPrices.add(extractPrice(list.get(i).getText()));
-//       }
-       
-       
-       //Printing the numbers here again to check the trimming
-//       for(int i=0;i<gPrices.size();i++) {
-//    	   System.out.println(gPrices.get(i));
-//       } 
-       
-       
        
        //Getting the current window  which currently is the google shopping page .
        String cWindow=driver.getWindowHandle();
        
-       //Extracting the product name here of the product before going to the next window.
-     //  String gProductName = driver.findElement(By.cssSelector("span.BvQan.sh-t__title-pdp.sh-t__title.translate-content")).getText();
-
-       
-          
-       //Here at this click another window opens which takes the user to the amazon showing the details of the same product.
-     //  driver.findElement(By.xpath("(//div[@class='Kl9jM UKKY9'])[1]")).click();
-       
-       
        
        //Creating a set to store all the windows available here
        Set<String> windowHandles = driver.getWindowHandles();
-       
-       // Create an iterator to loop through the window handles and once found the window which was not matching the current window
-       //meaning we went to the next window here because at  this point we only have 2 windows 
-//       for(String str:windowHandles) {
-//    	   if(str!=cWindow) {
-//    		   driver.switchTo().window(str);
-//    	   }
-//       }
        
        
        //Storing the window of the amazon here
@@ -146,72 +110,23 @@ public class Base {
        String amazonWindowTitle=driver.getTitle();
        System.out.println(driver.getTitle());
        
-       
-       
-       //Extracting the price of the product on the amazon here 
-      // String amazon=driver.findElement(By.xpath("//span[@class='a-price aok-align-center reinventPricePriceToPayMargin priceToPay']//span[@class='a-price-whole']")).getText();
-
-      
-       //trimming and parsing the extracted amazon price in the above step
-//       amazon= extractPrice(amazon);
-//       System.out.println(amazon);
-       
-       
+ 
        //Verifying the extract function to check how does it trim and process the numbers here
        String str="588,99,000.001";
        System.out.println(extractPrice(str));
        
-       //Extracting the product name here within the amazon
-    //   String productName=driver.findElement(By.xpath("//span[@id='productTitle']")).getText();
        
-       
-       //extracting the product names here in order to assert them in future
-    //   System.out.println(productName +"                "+gProductName);
-       
-       
-       //closing the window here trying to switch the window back to google shopping
-     //  driver.close();  // Close the second window
-       
-       
-
        // After closing the second window, WebDriver will automatically switch to the first window
        // You can now switch to the first window using the updated window handles
        Set<String> windowHandlesLatest = driver.getWindowHandles();
        
        windowHandlesLatest.remove(amazonWindow);
        
-       //Switching back to the google shopping window here by comparing the amazon window with google shopping .
-//       for (String windowHandle : windowHandlesLatest) {
-//    	    if (windowHandle!=amazonWindow) { // Switch to the window that isn't the current one
-//    	        driver.switchTo().window(windowHandle);
-//    	        break; // No need to continue looping once we've switched
-//    	    }
-//    	}
-        
+      
        //Verifying if the driver did switch in the above step by printing it
        System.out.println(driver.getTitle());
       
-//       Thread.sleep(2000);
-//       //clicking back until reach the landing page here
-//       driver.navigate().back();
-//       Thread.sleep(2000);
-//       driver.navigate().back();
-//       Thread.sleep(2000);
-      
-      
-       //Locating the on sale button in the google shopping page
-      // WebElement onSaleElement = driver.findElement(By.xpath("//span[@title='On sale']"));
-       
-       // Check if the element is selected before clicking
-      // System.out.println("Before click - isSelected: " + onSaleElement.isSelected());
-       
-       // Scroll to the element if needed
-      // js.executeScript("arguments[0].scrollIntoView({block: 'start', inline: 'nearest'})", onSaleElement);
-       //js.executeScript("window.scrollBy(0,-300);");
-
-       // Click the element (button, checkbox, or radio button)
-       //  onSaleElement.click();
-       
+     
        // Wait for the action to take effect (you may need to adjust this for your specific case)
        Thread.sleep(2000);  // You can also replace Thread.sleep() with WebDriverWait if you need dynamic waits
       
@@ -224,16 +139,7 @@ public class Base {
        //Printing the size of the products 
        System.out.println(childElements.size());	
        
-       //clicking upon amazon by scrolling down again 
-//       amazonInSeller=driver.findElement(By.cssSelector("span.lg3aE[title='Amazon.in']"));
-//       js.executeScript("arguments[0].scrollIntoView",amazonInSeller);
-//       js.executeScript("window.scrollBy(0,-300)");
-//        Thread.sleep(5000);
-        
-        //clicking upon the amazon button in sellers menu
-      //  driver.findElement(By.cssSelector("span.lg3aE[title='Amazon.in']")).click();
-       
-       
+      
   
        int c=0;
        for(int i=1;i<childElements.size();i++) {
@@ -245,42 +151,29 @@ public class Base {
     	  }
        }
        
-     Thread.sleep(1000);
-     driver.navigate().refresh();
-     //  WebElement priceFilter=driver.findElement(By.xpath("//div[text()='Price']"));
-       
-//    		   js.executeScript("arguments[0].scrollIntoView(true)", priceFilter);
-    		   Thread.sleep(2000);
-    		  // driver.findElement(By.xpath("(//g-text-field[@jsname='pp37Ef'])[2]")).click();
-    	        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-    	        WebElement inputField = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("(//div[@class='hUhkLe'])[2]")));
-
-    	        // Now interact with the element
-    	        inputField.click();
-    	        inputField.sendKeys("12000");
-
+                Thread.sleep(1000);
+                driver.navigate().refresh();
+                
     		   
-    		   //driver.findElement(By.xpath("(//div[@class='hUhkLe'])[2]")).sendKeys("12000");
-    		   driver.findElement(By.xpath("//button[@class='sh-dr__prs']")).click();
        
-      WebElement productsAfterPriceFilter = driver.findElement(By.xpath("//*[contains(@class, 'sh-pr__product-results-grid') and contains(@class, 'sh-pr__product-results')]"));
-      
-      //Storing all the products here within the list here
-      List<WebElement> childElementsAfterPriceFilter = productsAfterPriceFilter.findElements(By.xpath("./*"));
-       
-      System.out.println(childElementsAfterPriceFilter.size());
-      
-      int cc=0;
-      for(int i=1;i<childElementsAfterPriceFilter.size();i++) {
-   	   
-   	  switchWindowAndCompare(driver,i);
-   	  cc++;
-   	  if(cc==1) {
-   		  break;
-   	  }
-      }
+//      WebElement productsAfterPriceFilter = driver.findElement(By.xpath("//*[contains(@class, 'sh-pr__product-results-grid') and contains(@class, 'sh-pr__product-results')]"));
+//      
+//      //Storing all the products here within the list here
+//      List<WebElement> childElementsAfterPriceFilter = productsAfterPriceFilter.findElements(By.xpath("./*"));
+//       
+//      System.out.println(childElementsAfterPriceFilter.size());
+//      
+//      int cc=0;
+//      for(int i=1;i<childElementsAfterPriceFilter.size();i++) {
+//   	   
+//   	  switchWindowAndCompare(driver,i);
+//   	  cc++;
+//   	  if(cc==1) {
+//   		  break;
+//   	  }
+//      }
         
-	}
+	} 
 	
 	public static String extractPrice(String str) {
         // Step 1: Remove currency symbol (₹, $, etc.)
@@ -303,38 +196,25 @@ public class Base {
 	public static void switchWindowAndCompare(WebDriver driver, int productIndex) throws InterruptedException {
 		   
 		WebElement checkInScroll=driver.findElement(By.xpath("(//span[@class='OA4wid' and text()='Smartphone'])[" + productIndex + "]"));
-//		if(!isElementInViewport(driver,checkInScroll)) {
-//			JavascriptExecutor jss=(JavascriptExecutor)driver;
-//			jss.executeScript("arguments[0].scrollIntoView(true)", checkInScroll);
-//			Thread.sleep(4000);
-//	}
-		
-		
-	
-		
+
+				
 			driver.navigate().refresh();
 		
 		    Thread.sleep(5000);
 			//clicking upon the products card from the google shopping landing page
 		    driver.findElement(By.xpath("(//span[@class='OA4wid' and text()='Smartphone'])[" + productIndex + "]")).click(); 
-		    
 		    Thread.sleep(5000);
-		    WebElement viewMoreDetails=driver.findElement(By.xpath("(//span[@class='_-p3 _-pZ'])[1]"));
+		    WebElement viewMoreDetails=driver.findElement(By.xpath("(//span[@class='_-p2 _-pY'])[1]"));
 		    JavascriptExecutor jss=(JavascriptExecutor)driver;
 			jss.executeScript("arguments[0].scrollIntoView(true)", viewMoreDetails);
 			Thread.sleep(3000);
 		    jss.executeScript("window.scrollBy(0,-200);");
-			//Thread.sleep(5000);
+			Thread.sleep(5000);
 			viewMoreDetails.click();
-//			Actions act=new Actions(driver);
-//			act.moveToElement(driver.findElement(By.xpath("(//div[@class='_-pA'])[" + productIndex + "]"))).click().build().perform();
-//			
-			
-		    
-		   
-		   
 		
-			WebElement googleShoppingPrice =driver.findElement(By.xpath("(//span[@class='_-p3 _-pZ'])[1]"));
+			
+		    		
+			WebElement googleShoppingPrice =driver.findElement(By.xpath("(//span[@class='_-p2 _-pY'])[1]"));
 			System.out.println(googleShoppingPrice.getText()+"    "+"googleShoppingPrice here");
 			
 		     
@@ -343,15 +223,8 @@ public class Base {
 		   String cWindow=driver.getWindowHandle();
 		   
 		   Thread.sleep(5000);
-		   //Extracting the product name here from the google shopping product details page
-	      // String gProductName = driver.findElement(By.cssSelector("span.BvQan.sh-t__title-pdp.sh-t__title.translate-content")).getText();
-
-	       Thread.sleep(3000);
-	       //this is the click which takes the use to the amazon page from google shopping page to amazon page
-	      // driver.findElement(By.xpath("(//div[@class='Kl9jM UKKY9'])[1]")).click();
-	    //   driver.findElement(By.xpath("(//div[@class='Kl9jM UKKY9' and text()='Visit site'])[1]")).click();
-	       
-	       
+		  
+	           
 	       //creating set to store all the windows here in order to switch to the amazon site 
 	       Set<String> windowHandles = driver.getWindowHandles();
 	       
@@ -393,7 +266,7 @@ public class Base {
 	    	    }
 	    	}
 	       
-	     //  driver.navigate().back();
+	      // driver.navigate().back();
 	       Thread.sleep(5000);
 	       
 	       jss.executeScript("window.scrollBy(0,-200);");
