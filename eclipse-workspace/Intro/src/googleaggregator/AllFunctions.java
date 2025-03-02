cd4ab2edf826660ee4b10f6bafd3471a5a6e6ad1
sagarjhathi <sagarjhathii@gmail.com>
2025-03-02 23:31:03 +0530

    Adding All the funcitons in the function class and refractoing some things

--- eclipse-workspace/Intro/src/googleaggregator/AllFunctions.java
+++ eclipse-workspace/Intro/src/googleaggregator/AllFunctions.java
@@ -4,10 +4,12 @@ import java.time.Duration;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -91,13 +93,13 @@ public class AllFunctions {
     public static void applyingPriceFilter(WebDriver driver,List<WebElement> list) throws InterruptedException {
     	System.out.println("Applying the price filter then re-running the function");
         WebElement priceFilter=driver.findElement(By.xpath("(//span[@class='lg3aE']/span[contains(text(),'₹')])[2]"));
-        Base.applyFilterAndTraverse(driver, priceFilter, list);
+        applyFilterAndTraverse(driver, priceFilter, list);
     }
     
     public static void applyingRamFilter(WebDriver driver,List<WebElement> list) throws InterruptedException {
     	 System.out.println("Applying the ram filter then re-running the function");
          WebElement ram=driver.findElement(By.xpath("(//a[@class='vjtvke ch6u2b']//span[@class='lg3aE' and contains(@title, 'GB')])[5]"));
-         Base.applyFilterAndTraverse(driver, ram, list);
+         applyFilterAndTraverse(driver, ram, list);
     }
     
     
@@ -111,14 +113,14 @@ public class AllFunctions {
          moreFromProductRating.click();
          
          WebElement fourAndUp=driver.findElement(By.xpath("(//div[@class='vq3ore' and @aria-label='4 out of 5 stars'])[2]"));
-         Base.applyFilterAndTraverse(driver, fourAndUp, list);
+         applyFilterAndTraverse(driver, fourAndUp, list);
     }
     
     
     public static void applyingWeightfilter(WebDriver driver,List<WebElement> list ) throws InterruptedException {
     	System.out.println("Applying the weight filter then re-running the function");
         WebElement weight=driver.findElement(By.xpath("(//*[contains(text(), 'grams')])[1]"));
-        Base.applyFilterAndTraverse(driver, weight, list);
+        applyFilterAndTraverse(driver, weight, list);
     }
     
     
@@ -127,7 +129,7 @@ public class AllFunctions {
     public static void applyingDeliveryFilter(WebDriver driver,List<WebElement> list ) throws InterruptedException {
     	 System.out.println("Applying the delivery filter then re-running the function");
     	    WebElement delivery=driver.findElement(By.xpath("//span[@title='1–3 day delivery']"));
-    	    Base.applyFilterAndTraverse(driver, delivery, list);
+    	   applyFilterAndTraverse(driver, delivery, list);
     }
     
     
@@ -136,14 +138,14 @@ public class AllFunctions {
     public static void applyingBrandFilter(WebDriver driver,List<WebElement> list ) throws InterruptedException {
     	System.out.println("Applying the mobile brand  filter then re-running the function");
         WebElement mobileBrand=driver.findElement(By.xpath("//span[@title='Realme']"));
-        Base.applyFilterAndTraverse(driver, mobileBrand, list);
+        applyFilterAndTraverse(driver, mobileBrand, list);
    }
     
     
     public static void applying5gFilter(WebDriver driver,List<WebElement> list) throws InterruptedException {
     	System.out.println("Applying the 5G filter then re-running the function");
         WebElement selecting5G =driver.findElement(By.xpath("//span[@title='5G']"));
-        Base.applyFilterAndTraverse(driver, selecting5G, list);
+        applyFilterAndTraverse(driver, selecting5G, list);
         
     }
     
@@ -153,65 +155,65 @@ public class AllFunctions {
     public static void applyingDualSimFilter(WebDriver driver,List<WebElement> list) throws InterruptedException {
     	System.out.println("Applying the Dual Sim filter then re-running the function");
         WebElement dualSim=  driver.findElement(By.xpath("//span[@title='Dual SIM']"));
-        Base.applyFilterAndTraverse(driver, dualSim, list);
+        applyFilterAndTraverse(driver, dualSim, list);
     }
     
     
     public static void applyingColorFilter(WebDriver driver,List<WebElement> list) throws InterruptedException {
     	 System.out.println("Applying the color filter then re-running the function");
          WebElement color=driver.findElement(By.xpath("//span[text()='Black']"));
-         Base.applyFilterAndTraverse(driver, color, list);
+         applyFilterAndTraverse(driver, color, list);
     }
     
     
     public static void applyingStorageFilter(WebDriver driver,List<WebElement> list) throws InterruptedException {
     	 System.out.println("Applying the storage filter then re-running the function");
          WebElement storageCapacity =driver.findElement(By.xpath("//span[text()='64 GB']"));
-         Base.applyFilterAndTraverse(driver, storageCapacity, list);
+        applyFilterAndTraverse(driver, storageCapacity, list);
     }
     
     
     public static void applyingOsFilter(WebDriver driver,List<WebElement> list) throws InterruptedException {
     	 System.out.println("Applying the os filter then re-running the function");      
          WebElement os=driver.findElement(By.xpath("//span[text()='Android']"));
-         Base.applyFilterAndTraverse(driver, os, list);
+         applyFilterAndTraverse(driver, os, list);
     }
     
     
     public static void applyingCellularNetworkFilter(WebDriver driver,List<WebElement> list) throws InterruptedException {
     	System.out.println("Applying the cellular Network filter then re-running the function");
         WebElement cellularNetwork=driver.findElement(By.xpath("//span[@title='GSM Network']"));
-        Base.applyFilterAndTraverse(driver, cellularNetwork, list);
+        applyFilterAndTraverse(driver, cellularNetwork, list);
     }
     
     public static void applyingSecurityFeaturesFilter(WebDriver driver,List<WebElement> list) throws InterruptedException {
     	System.out.println("Applying the security features filter then re-running the function");
         WebElement securityFeatures=driver.findElement(By.xpath("//span[@title='Mobile Phones With Fingerprint Scanners']"));
-        Base.applyFilterAndTraverse(driver, securityFeatures, list);
+        applyFilterAndTraverse(driver, securityFeatures, list);
     }
     
     public static void applyingScreenResolutionFilter(WebDriver driver,List<WebElement> list) throws InterruptedException {
     	System.out.println("Applying the sreen resolution filter then re-running the function");      
         WebElement screenResolution = driver.findElement(By.xpath("//span[@title='2408 x 1080']"));
-        Base.applyFilterAndTraverse(driver, screenResolution, list);
+      applyFilterAndTraverse(driver, screenResolution, list);
     }
     
     public static void applyingCameraFilter(WebDriver driver,List<WebElement> list) throws InterruptedException {
     	System.out.println("Applying the camera filter then re-running the function");
         WebElement camera= driver.findElement(By.xpath("(//a[@class='vjtvke ch6u2b']//span[@class='lg3aE' and contains(@title, 'MP')])[1]"));
-        Base.applyFilterAndTraverse(driver, camera, list);
+        applyFilterAndTraverse(driver, camera, list);
     }
     
     public static void applyingHeadphoneFilter(WebDriver driver,List<WebElement> list) throws InterruptedException {
     	 System.out.println("Applying the headphone filter then re-running the function");
          WebElement headphoneConnector=driver.findElement(By.xpath("//span[@title='Headphone Jack']"));
-         Base.applyFilterAndTraverse(driver, headphoneConnector, list);
+        applyFilterAndTraverse(driver, headphoneConnector, list);
     }
     
     public static void applyingLensFilter(WebDriver driver,List<WebElement> list) throws InterruptedException {
     	 System.out.println("Applying the lens filter then re-running the function");
          WebElement lensType=driver.findElement(By.xpath("//span[@title='Ultra Wide Angle']"));
-         Base.applyFilterAndTraverse(driver, lensType, list);
+         applyFilterAndTraverse(driver, lensType, list);
     }
     
     public static void compareName(String str1, String str2) {
@@ -285,10 +287,10 @@ public class AllFunctions {
     
     public static void compareprice(String str1, String str2) {
     	
-    	System.out.println(Base.extractPrice(str1)+  "    "+   "Compare price str1 ");
+    	System.out.println(extractPrice(str1)+  "    "+   "Compare price str1 ");
     	
     	
-    	System.out.println(Base.extractPrice(str2)+  "    "+   "Compare price str2 ");
+    	System.out.println(extractPrice(str2)+  "    "+   "Compare price str2 ");
 
     	
     	
@@ -296,6 +298,228 @@ public class AllFunctions {
     }
     
     
+    public static String extractPrice(String str) {
+        // Step 1: Remove currency symbol (₹, $, etc.)
+        str = str.replaceAll("₹|\\$|€|£", "").trim(); // You can add more currency symbols if needed
+        
+        // Step 2: Remove decimal part, if present (i.e., remove anything after the dot)
+        if (str.contains(".")) {
+            str = str.substring(0, str.indexOf("."));
+        }
+        
+        // Step 3: Remove commas (thousands separators)
+        str = str.replace(",", "");
+            
+        // Return the cleaned up price
+        return str;
+    }
+    
+    
+    
+    public static void switchWindowAndCompare(WebDriver driver, int productIndex) throws InterruptedException {
+		 
+		driver.navigate().refresh();
+			Thread.sleep(1000);
+		WebElement productLocating=driver.findElement(By.xpath("(//span[@class='OA4wid' and text()='Smartphone'])[" + productIndex + "]"));
+
+		//clicking upon the products card from the google shopping landing page
+	    WebElement wt=new WebDriverWait(driver,Duration.ofSeconds(10)).until(ExpectedConditions.elementToBeClickable(productLocating));
+	   wt.click();
+
+	    
+	   
+	    WebElement viewMoreDetails=driver.findElement(By.xpath("//a[text()='View product details']"));
+	    JavascriptExecutor jss=(JavascriptExecutor)driver;
+		jss.executeScript("arguments[0].scrollIntoView(true)", viewMoreDetails);
+		Thread.sleep(1000);
+	    jss.executeScript("window.scrollBy(0,-200);");
+		Thread.sleep(1000);
+		viewMoreDetails.click();
+	
+		String str1=driver.findElement(By.xpath("//span[@role='heading']")).getText();
+		
+//		String googleViewMoreDetailsPrice=driver.findElement(By.xpath("(//span[@class='g9WBQb'])[1]")).getText();
+		
+		String googleViewMoreDetailsPrice="";
+		try{
+		googleViewMoreDetailsPrice=driver.findElement(By.xpath("(//span[@class='g9WBQb'])[1]")).getText();
+			
+		}catch(Exception e) {
+			System.out.println("The google  price is not available in Ui");
+			driver.quit();
+		}
+		
+		 System.out.println(googleViewMoreDetailsPrice+"     printing the google Price before processing ");
+		Thread.sleep(1000);
+		
+		String cWindow=driver.getWindowHandle();
+	     driver.findElement(By.xpath("(//div[@class='Kl9jM UKKY9'])[1]")).click();
+       
+       //storing the current window which is google shopping individual product details page
+	   
+	   
+	   Thread.sleep(1000);
+	  
+           
+       //creating set to store all the windows here in order to switch to the amazon site 
+       Set<String> windowHandles = driver.getWindowHandles();
+       
+       // Create an iterator to loop through the window handles and switch to the amazon site
+       for(String str:windowHandles) {
+    	   if(str!=cWindow) {
+    		  Thread.sleep(2000);
+    		   driver.switchTo().window(str);
+    	   }
+       }
+       
+       
+       
+       //Verifying how many windows here to check if we are accessing the right window here
+       for(String str:windowHandles) {
+    	   	System.out.println(str +"here here");
+       }
+      
+       
+       
+       String str2=driver.findElement(By.xpath("//span[@id='productTitle']")).getText();
+       String amazonPriceFromAmzon="";
+       
+       
+           try{
+    	   amazonPriceFromAmzon=driver.findElement(By.xpath("//span[@class='a-price aok-align-ce"
+   	       		+ "nter reinventPricePriceToPayMargin priceToPay']")).getText();
+			}catch(Exception e) {
+				System.out.println("The amazon price is not available in Ui");
+				driver.quit();
+			}
+       
+       System.out.println(amazonPriceFromAmzon+"     printing the amazonPrice before processing ");
+       
+       //Storing the amazon window here as the driver should be at the amazon site
+      String amazonWindow=driver.getWindowHandle();
+      Thread.sleep(2000);
+      
+      
+     // AllFunctions.compareName(str1,str2);
+      
+      AllFunctions.compareNameViaHashMapMethod(str1, str2);
+      
+      AllFunctions.compareprice(googleViewMoreDetailsPrice, amazonPriceFromAmzon);
+      
+      
+      
+      // Extracting the price from Amazon
+      try {
+          WebDriverWait checkOne = new WebDriverWait(driver, Duration.ofSeconds(10));
+          WebElement amazonPrice = checkOne.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[@class='a-price aok-align-center"
+                  + " reinventPricePriceToPayMargin priceToPay']//span[@class='a-price-whole']")));
+
+          if (amazonPrice != null && amazonPrice.isDisplayed() == false) {
+              System.out.println("The element is not available on the UI.");
+          }
+
+      } catch (Exception e) {
+          System.out.println("Element not found or not visible within the time limit.");
+          // Optionally, add code to skip this step or proceed with the next actions
+      }
+
+      // Extracting the product name from Amazon
+      String productName = "";
+      try {
+          productName = driver.findElement(By.xpath("//span[@id='productTitle']")).getText();
+      } catch (Exception e) {
+          System.out.println("Product name not found on Amazon.");
+          // You can leave productName empty or handle it differently
+      }
+      
+      if (!productName.isEmpty()) {
+          System.out.println("Product Name: " + productName);
+      }
+
+       
+       
+       Set<String> windowHandlesLatest = driver.getWindowHandles();
+       
+       driver.close();
+       
+       for (String windowHandle : windowHandlesLatest) {
+    	    if (windowHandle!=amazonWindow) { // Switch to the window that isn't the current one
+    	        driver.switchTo().window(windowHandle);
+    	        break; // No need to continue looping once we've switched
+    	    }
+    	}
+       
+       driver.navigate().back();
+       Thread.sleep(2000);
+       
+       jss.executeScript("window.scrollBy(0,-200);");
+       Thread.sleep(2000);
+       driver.findElement(By.xpath("//a[@aria-label='Close']")).click();
+       
+
+}
+    
+    
+    
+    public static boolean isElementInViewport(WebDriver driver, WebElement element) {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        // JavaScript to check if the element is in the viewport
+        String script = "var elem = arguments[0], " +
+                        "bounding = elem.getBoundingClientRect(), " +
+                        "vPwidth = window.innerWidth || document.documentElement.clientWidth, " +
+                        "vPheight = window.innerHeight || document.documentElement.clientHeight, " +
+                        "vTop = bounding.top >= 0 && bounding.top < vPheight, " +
+                        "vLeft = bounding.left >= 0 && bounding.left < vPwidth; " +
+                        "return vTop && vLeft;";
+        return (Boolean) js.executeScript(script, element);
+    }
+    
+    
+    
+    public static void applyFilterAndTraverse(WebDriver driver, WebElement element, List<WebElement> list) throws InterruptedException {
+		 JavascriptExecutor js=(JavascriptExecutor)driver;
+		 js.executeScript("arguments[0].scrollIntoView(true)", element);
+	      js.executeScript("window.scrollBy(0,-300);");
+	      Thread.sleep(2000);
+	      element.click();
+	      
+	     
+	      
+	      try {
+	          WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+	          // Wait for the "No results" state element to be visible, if it appears
+	          WebElement noResults = wait.until(ExpectedConditions.visibilityOfElementLocated(
+	                  By.xpath("//ul[@style='margin-left:1.3em;margin-bottom:2em']")
+	          ));
+
+	          if (noResults.isDisplayed()) {
+	              System.out.println("No results found, navigating back...");
+	              driver.navigate().back();  // Navigate back before returning
+	              return;  // Exit the method if no results are found
+	          }
+	      } catch (TimeoutException e) {
+	          // No "No results" state found, continue with the normal actions
+	          System.out.println("Results found, proceeding with further actions.");
+	      }
+	    	    
+	    	        int c = 0;
+	    	        for (int i = 1; i < list.size(); i++) {
+	    	            switchWindowAndCompare(driver, i);
+	    	            c++;
+	    	            
+	    	            // Break after the first iteration
+	    	            if (c == 1) {
+	    	                break;
+	    	            }
+	    	        }
+	    	        
+	    	        // Perform further actions after comparison
+	    	        Thread.sleep(1000);
+	    	        driver.findElement(By.xpath("(//a[@class='vjtvke' and text()='Clear'])[1]")).click();
+	    	        Thread.sleep(1000);
+	         
+	 }
     
 //    public static void comparePrice(WebDriver driver, String str1, String str2) {
 //        // Find all "view more details" links (or elements that might contain the text you're looking for)
---
+++ eclipse-workspace/Intro/src/googleaggregator/Tests.java
@@ -0,0 +1,12 @@
+package googleaggregator;
+
+import org.testng.annotations.Test;
+
+public class Tests {
+
+	
+	@Test
+	public  void demo() {
+		System.out.println("Hello");
+	}
+}
