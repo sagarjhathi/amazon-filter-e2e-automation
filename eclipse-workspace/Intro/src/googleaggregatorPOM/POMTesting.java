65186d44affeab5fb325808116d872b18cdc340d
sagarjhathi <sagarjhathii@gmail.com>
2025-04-06 00:36:30 +0530

    Pushing code for trial , running all filter options code

--- eclipse-workspace/Intro/src/googleaggregatorPOM/POMTesting.java
+++ eclipse-workspace/Intro/src/googleaggregatorPOM/POMTesting.java
@@ -17,14 +17,15 @@ public class POMTesting {
 	GenericHelper genericHelper;
 	GoogleShoppingMainPage googleShoppingMainPage;
 	GoogleShoppingLandingPage  	googleShoppingLandingPage;
-	
+	DriverManager driverManager;
 	
 	
 	@BeforeClass
     public void setup() {
 		 this.globalUtility = new GlobalUtility(driver); 
 		this.genericHelper=new GenericHelper();
-		this.driver=globalUtility.initDriver(genericHelper.userAgent);
+		this.driverManager=new DriverManager();
+		this.driver=driverManager.setDriver(globalUtility.initDriver(genericHelper.userAgent));
 		this.googleShoppingLandingPage=new GoogleShoppingLandingPage(driver);
 		 this.googleShoppingMainPage = new GoogleShoppingMainPage(driver, globalUtility);
     }
@@ -80,103 +81,104 @@ public class POMTesting {
 	@Test(priority=8)
 	public void clickingOnAmazonFromSellerMenu() {
 		googleShoppingMainPage.selectingAmazonFromSellerMenu(driver);
+	//	googleShoppingMainPage.locateAllFiltersAndApplyEachFilterWithAlloptions(driver);
 	}
 	
+	
+//	@Test(priority = 9)
+//	public void applyingScreenResolutionFilterInGoogleMainPage() throws InterruptedException {
+//		googleShoppingMainPage.applyingScreenResolutionFilter(driver, googleShoppingMainPage.gettingAllProducts(driver));
+//	}
+//	
+//	
+//	@Test(priority = 10)
+//	public void applyingWeightFilterInGoogleMainPage() throws InterruptedException {
+//		googleShoppingMainPage.applyingWeightFilter(driver, googleShoppingMainPage.gettingAllProducts(driver));
+//	}
+//	
+//	
+//
+//	@Test(priority = 11)
+//	public void applyingSecurityFeaturesFilterInGoogleMainPage() throws InterruptedException {
+//		googleShoppingMainPage.applyingSecurityFeaturesFilter(driver, googleShoppingMainPage.gettingAllProducts(driver));
+//	}
+//
+//	@Test(priority = 12)
+//	public void applyingStorageFilterInGoogleMainPage() throws InterruptedException {
+//		googleShoppingMainPage.applyingStorageFilter(driver, googleShoppingMainPage.gettingAllProducts(driver));
+//	}
+//	
+//	@Test(priority = 13)
+//	public void applyingPriceFilterInGoogleMainPage() throws InterruptedException {
+//	    googleShoppingMainPage.applyingPriceFilter(driver, googleShoppingMainPage.gettingAllProducts(driver));
+//	}
+//
+//	@Test(priority = 14)
+//	public void applying5gFilterInGoogleMainPage() throws InterruptedException {
+//	    googleShoppingMainPage.applying5GFilter(driver, googleShoppingMainPage.gettingAllProducts(driver));
+//	}
+//
+//	@Test(priority = 15)
+//	public void applyingBrandFilterInGoogleMainPage() throws InterruptedException {
+//		googleShoppingMainPage.applyingBrandFilter(driver, googleShoppingMainPage.gettingAllProducts(driver));
+//	}
+//
+//	@Test(priority = 16)
+//	public void applyingCameraFilterInGoogleMainPage() throws InterruptedException {
+//		googleShoppingMainPage.applyingCameraFilter(driver, googleShoppingMainPage.gettingAllProducts(driver));
+//	}
+//
+//	@Test(priority = 17)
+//	public void applyingCellularNetworkFilterInGoogleMainPage() throws InterruptedException {
+//		googleShoppingMainPage.applyingCellularNetworkFilter(driver, googleShoppingMainPage.gettingAllProducts(driver));
+//	}
+//
+//	@Test(priority = 18)
+//	public void applyingColorFilterInGoogleMainPage() throws InterruptedException {
+//		googleShoppingMainPage.applyingColorFilter(driver, googleShoppingMainPage.gettingAllProducts(driver));
+//	}
+//
+//	@Test(priority = 19)
+//	public void applyingDeliveryFilterInGoogleMainPage() throws InterruptedException {
+//		googleShoppingMainPage.applyingDeliveryFilter(driver, googleShoppingMainPage.gettingAllProducts(driver));
+//	}
+//
+//	@Test(priority = 20)
+//	public void applyingDualSimFilterInGoogleMainPage() throws InterruptedException {
+//		googleShoppingMainPage.applyingDualSimFilter(driver, googleShoppingMainPage.gettingAllProducts(driver));
+//	}
+//
+//	@Test(priority = 21)
+//	public void applyingHeadphoneFilterInGoogleMainPage() throws InterruptedException {
+//		googleShoppingMainPage.applyingHeadphoneFilter(driver, googleShoppingMainPage.gettingAllProducts(driver));
+//	}
+//
+//	@Test(priority = 22)
+//	public void applyingLensFilterInGoogleMainPage() throws InterruptedException {
+//		googleShoppingMainPage.applyingLensFilter(driver, googleShoppingMainPage.gettingAllProducts(driver));
+//	}
+//
+//	@Test(priority = 23)
+//	public void applyingOsFilterInGoogleMainPage() throws InterruptedException {
+//		googleShoppingMainPage.applyingOsFilter(driver, googleShoppingMainPage.gettingAllProducts(driver));
+//	}
+//
+//	@Test(priority = 24)
+//	public void applyingRamFilterInGoogleMainPage() throws InterruptedException {
+//		googleShoppingMainPage.applyingRamFilter(driver, googleShoppingMainPage.gettingAllProducts(driver));
+//	}
+//
+//	@Test(priority = 25)
+//	public void applyingRatingFilterInGoogleMainPage() throws InterruptedException {
+//		googleShoppingMainPage.applyingRatingFilter(driver, googleShoppingMainPage.gettingAllProducts(driver));
+//	}
+
 	
 	@Test(priority = 9)
-	public void applyingScreenResolutionFilterInGoogleMainPage() throws InterruptedException {
-		googleShoppingMainPage.applyingScreenResolutionFilter(driver, googleShoppingMainPage.gettingAllProducts(driver));
-	}
-	
-	
-	@Test(priority = 10)
-	public void applyingWeightFilterInGoogleMainPage() throws InterruptedException {
-		googleShoppingMainPage.applyingWeightFilter(driver, googleShoppingMainPage.gettingAllProducts(driver));
-	}
-	
-	
-
-	@Test(priority = 11)
-	public void applyingSecurityFeaturesFilterInGoogleMainPage() throws InterruptedException {
-		googleShoppingMainPage.applyingSecurityFeaturesFilter(driver, googleShoppingMainPage.gettingAllProducts(driver));
+	public void applyingAllFilterOptionsForPrice() throws InterruptedException {
+		googleShoppingMainPage.applyingPriceFilterAllOptions(driver);
 	}
 
-	@Test(priority = 12)
-	public void applyingStorageFilterInGoogleMainPage() throws InterruptedException {
-		googleShoppingMainPage.applyingStorageFilter(driver, googleShoppingMainPage.gettingAllProducts(driver));
-	}
-	
-	@Test(priority = 13)
-	public void applyingPriceFilterInGoogleMainPage() throws InterruptedException {
-	    googleShoppingMainPage.applyingPriceFilter(driver, googleShoppingMainPage.gettingAllProducts(driver));
-	}
-
-	@Test(priority = 14)
-	public void applying5gFilterInGoogleMainPage() throws InterruptedException {
-	    googleShoppingMainPage.applying5GFilter(driver, googleShoppingMainPage.gettingAllProducts(driver));
-	}
-
-	@Test(priority = 15)
-	public void applyingBrandFilterInGoogleMainPage() throws InterruptedException {
-		googleShoppingMainPage.applyingBrandFilter(driver, googleShoppingMainPage.gettingAllProducts(driver));
-	}
-
-	@Test(priority = 16)
-	public void applyingCameraFilterInGoogleMainPage() throws InterruptedException {
-		googleShoppingMainPage.applyingCameraFilter(driver, googleShoppingMainPage.gettingAllProducts(driver));
-	}
-
-	@Test(priority = 17)
-	public void applyingCellularNetworkFilterInGoogleMainPage() throws InterruptedException {
-		googleShoppingMainPage.applyingCellularNetworkFilter(driver, googleShoppingMainPage.gettingAllProducts(driver));
-	}
-
-	@Test(priority = 18)
-	public void applyingColorFilterInGoogleMainPage() throws InterruptedException {
-		googleShoppingMainPage.applyingColorFilter(driver, googleShoppingMainPage.gettingAllProducts(driver));
-	}
-
-	@Test(priority = 19)
-	public void applyingDeliveryFilterInGoogleMainPage() throws InterruptedException {
-		googleShoppingMainPage.applyingDeliveryFilter(driver, googleShoppingMainPage.gettingAllProducts(driver));
-	}
-
-	@Test(priority = 20)
-	public void applyingDualSimFilterInGoogleMainPage() throws InterruptedException {
-		googleShoppingMainPage.applyingDualSimFilter(driver, googleShoppingMainPage.gettingAllProducts(driver));
-	}
-
-	@Test(priority = 21)
-	public void applyingHeadphoneFilterInGoogleMainPage() throws InterruptedException {
-		googleShoppingMainPage.applyingHeadphoneFilter(driver, googleShoppingMainPage.gettingAllProducts(driver));
-	}
-
-	@Test(priority = 22)
-	public void applyingLensFilterInGoogleMainPage() throws InterruptedException {
-		googleShoppingMainPage.applyingLensFilter(driver, googleShoppingMainPage.gettingAllProducts(driver));
-	}
-
-	@Test(priority = 23)
-	public void applyingOsFilterInGoogleMainPage() throws InterruptedException {
-		googleShoppingMainPage.applyingOsFilter(driver, googleShoppingMainPage.gettingAllProducts(driver));
-	}
-
-	@Test(priority = 24)
-	public void applyingRamFilterInGoogleMainPage() throws InterruptedException {
-		googleShoppingMainPage.applyingRamFilter(driver, googleShoppingMainPage.gettingAllProducts(driver));
-	}
-
-	@Test(priority = 25)
-	public void applyingRatingFilterInGoogleMainPage() throws InterruptedException {
-		googleShoppingMainPage.applyingRatingFilter(driver, googleShoppingMainPage.gettingAllProducts(driver));
-	}
-
-
-//	@Test(priority = 9)
-//	public void applyingFiltersFromTheList() throws InterruptedException {
-//		googleShoppingMainPage.applyingAllFiltersFromList(driver);
-//	}
-	
 	
 	
 	
