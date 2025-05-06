52d99488defaa13170be9303e8010084a0581b30
sagarjhathi <sagarjhathii@gmail.com>
2025-05-06 20:19:00 +0530

    Adding amazon e2e filter application

---
+++ eclipse-workspace/Intro/src/amazonfilterapplicatione2e/AmazonLandingPage.java
@@ -0,0 +1,35 @@
+package amazonfilterapplicatione2e;
+
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class AmazonLandingPage {
+
+	
+	
+	@FindBy(xpath="//input[@id='twotabsearchtextbox']")
+	WebElement searchBarLandingPage;
+	
+	@FindBy(xpath="//input[@id='nav-search-submit-button']")
+	WebElement submitSearchButton;
+	
+	
+	public void givingInputWithinSearchBar(String input) {
+		searchBarLandingPage.click();
+		searchBarLandingPage.sendKeys(input);
+	}
+	
+	
+	public void selectingFromRecommendations() {
+		searchBarLandingPage.sendKeys(Keys.DOWN);
+		searchBarLandingPage.sendKeys(Keys.DOWN);	
+	}
+	
+	
+	public void clickingOnSubmitSearchButton() {
+		submitSearchButton.click();
+	}
+	
+	
+}
---
+++ eclipse-workspace/Intro/src/amazonfilterapplicatione2e/ProductListingPage.java
@@ -0,0 +1,8 @@
+package amazonfilterapplicatione2e;
+
+public class ProductListingPage {
+
+	//@FindBy(xpath="//span[@class='a-size-base a-color-base' and text()='Get It by Tomorrow']")
+	
+	
+}
