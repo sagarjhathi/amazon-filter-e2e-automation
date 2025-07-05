c18262cb6f14f411dddb89a1fa2483ab716eeab2
sagarjhathi <sagarjhathii@gmail.com>
2025-07-06 00:53:25 +0530

    Trying parallel execution and logging

--- eclipse-workspace/Intro/src/amazonfilterapplicatione2e/BaseTest.java
+++ eclipse-workspace/Intro/src/amazonfilterapplicatione2e/BaseTest.java
@@ -1,31 +1,68 @@
 package amazonfilterapplicatione2e;
 
-import org.apache.logging.log4j.LogManager;
+
+import java.lang.reflect.Method;
+
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.ThreadContext;
 import org.openqa.selenium.WebDriver;
+import org.testng.ITestContext;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.apache.logging.log4j.ThreadContext;
 
-import io.opentelemetry.api.logs.Logger;
+
 
 public class BaseTest {
  
-	
+	private static final Logger log = LoggerUtility.getLogger(BaseTest.class);
+
 	
 	 protected WebDriver driver;
 
-	    @BeforeMethod
-	    public void setUp() {
-	        // Initialize WebDriver instance using DriverManager
-	    	 DriverManager.initDriver(); 
-	        driver = DriverManager.getDriver();
-	    }
+//	    @BeforeMethod
+//	    public void setUp() {
+//	        // Initialize WebDriver instance using DriverManager
+//	    	DriverManager.initDriver(); 
+//	        driver = DriverManager.getDriver();
+//	    }
+//
+//	    @AfterMethod
+//	    public void tearDown() {
+//	        // Quit WebDriver instance after test
+//	        DriverManager.quitDriver();
+//	    }
+	 
+	 @BeforeMethod(alwaysRun = true)
+	 public void setup(Method method, ITestContext context) {
+	     // Set ThreadContext first
+	     String methodName = method.getName();
+	     String threadName = methodName + "-" + Thread.currentThread().threadId();
+	     ThreadContext.put("threadName", threadName);
+	     log.info("🔹 Starting test method: " + methodName);
 
-	    @AfterMethod
-	    public void tearDown() {
-	        // Quit WebDriver instance after test
-	        DriverManager.quitDriver();
-	    }
+	     // Then initialize driver
+	     DriverManager.initDriver(); 
+	     driver = DriverManager.getDriver();
+	 }
+
+	 
+	 @AfterMethod(alwaysRun = true)
+	 public void tearDown(ITestResult result) {
+	     log.info("✅ Finished test method: " + result.getName());
+	     DriverManager.quitDriver();
+	     ThreadContext.clearAll();
+	 }
+
 	
+	   
+	    
+	   
+
+
+	  
+
 	
 	
 }
