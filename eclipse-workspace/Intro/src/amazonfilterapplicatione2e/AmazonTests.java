87ebb5b2b80d9a2615f2bbcfd33b3a8c2f2db5cb
sagarjhathi <sagarjhathii@gmail.com>
2025-08-06 00:33:02 +0530

    Removed redundunt lines from amazon tests

--- eclipse-workspace/Intro/src/amazonfilterapplicatione2e/AmazonTests.java
+++ eclipse-workspace/Intro/src/amazonfilterapplicatione2e/AmazonTests.java
@@ -44,10 +44,10 @@ public class AmazonTests extends BaseTest {
 	public Object[][] getBrands() {
 	    return new Object[][] {
 	        {"Mobile"},
-	        {"Tv"},
-	        {"Charger"},
-	        {"AC"},
-	        {"Fridge"}
+//	        {"Tv"},
+//	        {"Charger"},
+//	        {"AC"},
+//	        {"Fridge"}
 	    };
 	}
 	
@@ -88,7 +88,7 @@ public class AmazonTests extends BaseTest {
 		}
 	
 	
-//	//@Test(priority=-2)
+
 	@Test(priority=2, retryAnalyzer = RetryFailedTest.class,dataProvider = "Data")
 	public void verifyingGetItIn2DaysFilterFunctionality(String input) throws InterruptedException{
 		
@@ -123,7 +123,7 @@ public class AmazonTests extends BaseTest {
 	
 
 
-//	//@Test(priority=-3)
+
 	@Test(priority = 3, retryAnalyzer = RetryFailedTest.class,dataProvider = "Data")
 	public void verifyingGetItByTodayFilterFunctionality(String input) throws InterruptedException{
 		
@@ -161,7 +161,8 @@ public class AmazonTests extends BaseTest {
 		            		
 		            		  
 	
-	//@Test(priority=-4)
+	
+	
 	@Test(priority=4, retryAnalyzer = RetryFailedTest.class,dataProvider = "Data")
 	public void verifyingTheBrandsFilterFunctionality(String input) throws InterruptedException {
 		
@@ -213,7 +214,7 @@ public class AmazonTests extends BaseTest {
 	}
 	
 	
-//	//@Test(priority=-5)
+
 	@Test(priority=5, retryAnalyzer = RetryFailedTest.class)
 	public void verifyingStorageCapacityFilterFunctionality() throws InterruptedException {
 		
@@ -271,7 +272,7 @@ public class AmazonTests extends BaseTest {
 	}
 
 	
-//	//@Test(priority=-6)
+
 	@Test(priority=6, retryAnalyzer = RetryFailedTest.class)
 	public void verifyingPriceSilderFunctionality() throws InterruptedException {
 		
@@ -327,7 +328,7 @@ public class AmazonTests extends BaseTest {
 	}
 
 	
-//	//@Test(priority=-7)
+
 	@Test(priority = 7, retryAnalyzer = RetryFailedTest.class)
 	public void verifyingBatteryCapacityFilterFunctionality() throws InterruptedException {
 
@@ -381,7 +382,7 @@ public class AmazonTests extends BaseTest {
 	}
 
 	
-//	//@Test(priority=8)
+
 	@Test(priority=8, retryAnalyzer = RetryFailedTest.class)
 	public void verifyingDisplaySizeFilterFunctionality() throws InterruptedException {
 		
@@ -437,7 +438,7 @@ public class AmazonTests extends BaseTest {
 	}
 
 	
-//	//@Test(priority=9)
+
 	@Test(priority=9, retryAnalyzer = RetryFailedTest.class)
 	public void verifyingProcessorSpeedFilterFunctionality() throws InterruptedException {
 				
@@ -491,7 +492,7 @@ public class AmazonTests extends BaseTest {
 	}
 
 	
-//	//@Test(priority=10)
+
 	@Test(priority=10, retryAnalyzer = RetryFailedTest.class)
 	public void verifyingDisplayTypeFilterFunctionality() throws InterruptedException {
 		
@@ -544,8 +545,8 @@ public class AmazonTests extends BaseTest {
 		}
 
 	
-//	@Test(priority=11)
-	//@Test(priority=11, retryAnalyzer = RetryFailedTest.class)
+
+	@Test(priority=11, retryAnalyzer = RetryFailedTest.class)
 	public void verifyingOperatingSystemVersionFilterFunctionality() throws InterruptedException {
 		
 		AmazonLandingPage am=new AmazonLandingPage();
@@ -599,8 +600,8 @@ public class AmazonTests extends BaseTest {
 	}
 
 	
-//	@Test(priority=12)
-	//@Test(priority=12, retryAnalyzer = RetryFailedTest.class)
+
+	@Test(priority=12, retryAnalyzer = RetryFailedTest.class)
 	public void verifyingMobilePhonePrimaryCameraResolutionFilterFunctionality() throws InterruptedException {
 		
 		AmazonLandingPage am=new AmazonLandingPage();
@@ -654,7 +655,7 @@ public class AmazonTests extends BaseTest {
 	}
 
 	
-//	@Test(priority=13)
+
 	@Test(priority=13, retryAnalyzer = RetryFailedTest.class)
 	public void verifyingDiscountFilterFunctionality() throws InterruptedException {
 		
