5ae24401ae99f8861e6fbcf4837e238c31b5723c
sagarjhathi <sagarjhathii@gmail.com>
2025-12-01 23:35:45 +0530

    Further more redundunt lines removed

--- eclipse-workspace/Intro/src/main/java/amazonfilterapplicatione2e/base/BaseTest.java
+++ eclipse-workspace/Intro/src/main/java/amazonfilterapplicatione2e/base/BaseTest.java
@@ -1,5 +1,5 @@
 package main.java.amazonfilterapplicatione2e.base;
-import java.lang.reflect.Method;
+import java.lang.reflect.Method; 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.ThreadContext;
@@ -11,41 +11,35 @@ import org.testng.annotations.Listeners;
 import main.java.amazonfilterapplicatione2e.driverManager.DriverManager;
 import main.java.amazonfilterapplicatione2e.logger.LoggerUtility;
 
-
-
-
 @Listeners(main.java.amazonfilterapplicatione2e.reporting.TestListener.class)
 public class BaseTest {
  
 	private   Logger log = LoggerUtility.getLogger(BaseTest.class);
 
+	protected WebDriver driver;
+	@BeforeMethod
+	public void setUp(Method method) {
 
-	
-	 protected WebDriver driver;
+		String testName = method.getName(); // The actual test method name
+		ThreadContext.put("testName", testName);    
+		ThreadContext.put("logFileName", testName); // must come before logger is called
+		log = LogManager.getLogger(testName); 
+		
+		System.out.println("🧪 logFileName: " + ThreadContext.get("logFileName"));	    	   
+		log.info("🔹 Starting test method: " + testName);
 
-	    @BeforeMethod
-	    public void setUp(Method method) {
-	        
-	    	    String testName = method.getName(); // The actual test method name
-	    	    String logName = method.getName() + "_" + Thread.currentThread().getId();
-	    	    ThreadContext.put("testName", testName);     // ✅ Add this for use in logs
-	    	    ThreadContext.put("logFileName", testName); // ✅ must come before logger is called
-	    	    log = LogManager.getLogger(testName); 
-	    	    System.out.println("🧪 logFileName: " + ThreadContext.get("logFileName"));	    	   
-	    	    log.info("🔹 Starting test method: " + testName);
-	    	    
-	        DriverManager.initDriver();
-	        driver = DriverManager.getDriver();
-	    }
-	 
-	    @AfterMethod
-	    public void tearDown(ITestResult result) {
-	        log.info("✅ Finished test method: " + result.getName());
-	        ThreadContext.clearAll();  // ✅ Critical to avoid context bleed
-	        DriverManager.quitDriver();
-	    }
-	   
-	   
+		DriverManager.initDriver();
+		driver = DriverManager.getDriver();
+	}
+
+	@AfterMethod
+	public void tearDown(ITestResult result) {
+		log.info("✅ Finished test method: " + result.getName());
+		ThreadContext.clearAll();  //Critical to avoid context bleed
+		DriverManager.quitDriver();
+	}
+
+
 	
 	
 	
